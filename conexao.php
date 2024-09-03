f7a0c9e5aebace05bb5d697a751eaa85a923434d
Julio9660 <jcrn@aluno.ifnmg.edu.br>
2024-09-03 08:58:21 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'sql202.infinityfree.com'); // ou '127.0.0.1'
 define('DB_USER', 'if0_37235531');
 define('DB_PASS', 'Jcrn2619'); // deixe vazio se você não definiu uma senha
-define('DB_NAME', 'componentes'); // substitua pelo nome do seu banco de dados
+define('DB_NAME', 'if0_37235531_componentes'); // substitua pelo nome do seu banco de dados
 
 // Criando a conexão
 $conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
